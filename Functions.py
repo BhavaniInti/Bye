127c97e6cb5fc2361a6abe603df6b0e8f8d992fb
BhavaniInti <durgainti068@gmail.com>
2025-12-17 07:51:05 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,11 @@
+def First():
+    print("My function in python")
+First()
+
+def Add(a,b):
+    print(f"Addtion of two numbers: {a+b}")
+    def Sub():
+        print(f"Subtraction of two numbers: {a-b}")
+    Sub()
+
+Add(3,6)
